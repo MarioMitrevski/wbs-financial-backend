dad9a5b48b330970ab480d020b41aebd6c15a555
MarioMitrevskii <mario.mitrevski@intertec.io>
2022-05-21 22:28:15 +0200

    Updated spring boot version & added feign client dependency

--- src/main/kotlin/com/example/wbsfinancialbackend/WbsFinancialBackendApplication.kt
+++ src/main/kotlin/com/example/wbsfinancialbackend/WbsFinancialBackendApplication.kt
@@ -3,11 +3,13 @@ package com.example.wbsfinancialbackend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
+@EnableFeignClients
 class WbsFinancialBackendApplication
 
 fun main(args: Array<String>) {
